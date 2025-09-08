7ae457dd7a5d9e456afd8f5b498b60b66499575c
TalhaShaikh150 <tssmart233@gmail.com>
2025-09-08 22:49:10 +0500

    - Added smooth transition effect to navbar - Enhanced hover animation for 'Let's Talk' button - Made hero section fully responsive

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,20 @@
- const scroll = new LocomotiveScroll({
-        el: document.querySelector("[data-scroll-container]"),
-        smooth: true,
-        multiplier: 3,
-      });
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+ multiplier: 1.5,
+ lerp: 0.1
+});
+
+const navbar = document.querySelector('.navbar');
+
+function navbarScroll() {
+  scroll.on('scroll', (obj) => {
+    if (obj.scroll.y > 50) { // change after 50px scroll
+      navbar.classList.add('scrolled'); // you can also add a class
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+}
+
+navbarScroll();
