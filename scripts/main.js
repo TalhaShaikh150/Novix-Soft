2b0c31cd01ef247e6b3ccc35957a67dd3abba886
TalhaShaikh150 <tssmart233@gmail.com>
2025-09-11 19:55:28 +0500

    Fixed Section 3 Styling and Started Section 4

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,18 @@
 const scroll = new LocomotiveScroll({
   el: document.querySelector("[data-scroll-container]"),
   smooth: true,
- multiplier:0.5,
+ multiplier:0.3 ,
+//  lerp:0.3
 });
 
-const navbar = document.querySelector('.navbar');
+
+function loadPage(){
+  switchSlides()
+  navbarScroll();
+}
 
 function navbarScroll() {
+  const navbar = document.querySelector('.navbar');
   scroll.on('scroll', (obj) => {
     if (obj.scroll.y > 50) { // change after 50px scroll
       navbar.classList.add('scrolled'); // you can also add a class
@@ -16,21 +22,27 @@ function navbarScroll() {
   });
 }
 
-navbarScroll();
 
 
+function switchSlides(){
 
-const showcase = document.querySelector(".section3-showcase");
-const leftBtn = document.querySelector(".left-arrow");
-const rightBtn = document.querySelector(".right-arrow");
-
-const workItem = document.querySelector(".work");
-const scrollAmount = workItem.offsetWidth + 20;
-
-rightBtn.addEventListener("click", () => {
-  showcase.scrollBy({ left: scrollAmount, behavior: "smooth" });
+  const showcase = document.querySelector(".section3-showcase");
+  const leftBtn = document.querySelector(".left-arrow");
+  const rightBtn = document.querySelector(".right-arrow");
+  
+  const workItem = document.querySelector(".work");
+  const scrollAmount = workItem.offsetWidth + 20;
+  
+  rightBtn.addEventListener("click", () => {
+    showcase.scrollBy({ left: scrollAmount, behavior: "smooth" });
+  });
+  
+  leftBtn.addEventListener("click", () => {
+    showcase.scrollBy({ left: -scrollAmount, behavior: "smooth" });
 });
+}
 
-leftBtn.addEventListener("click", () => {
-  showcase.scrollBy({ left: -scrollAmount, behavior: "smooth" });
-});
+
+document.addEventListener('DOMContentLoaded',()=>{
+  loadPage()
+})
